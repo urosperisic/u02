868c13992e37ac5849c9c279ae4be4e3b45b956d
up <uros2907@gmail.com>
2020-03-23 19:33:13 +0100

    Change addBook method II

--- src/main/java/io/u02/mapper/BookMapper.java
+++ src/main/java/io/u02/mapper/BookMapper.java
@@ -3,7 +3,6 @@ package io.u02.mapper;
 import io.u02.entity.BookEntity;
 import io.u02.model.Book;
 import org.mapstruct.Mapper;
-//import org.mapstruct.Mapping;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -11,7 +10,6 @@ import java.util.List;
 @Component
 @Mapper(componentModel ="spring")
 public interface BookMapper {
-    //@Mapping(target = "id", ignore = true)
     BookEntity mapToEntity(Book book);
     List<BookEntity> mapToEntity(List<Book> books);
     Book mapToModel(BookEntity entity);
